2ccf162fddad4b95cbcb92eada2d39a374ca8873
Gustavoph <gusta.coliveira07@gmail.com>
2021-06-25 21:36:44 -0300

    Conceitos iniciais sobre query

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+import { ApolloServer, gql } from 'apollo-server';
+const port = 4003;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return `Hello World`;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(port).then(({ url }) => {
+  console.log(`Server listening on url ${url}`);
+});
