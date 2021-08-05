479ed86b1cfff1330e0aae4b13c4c6b6834b01be
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2021-08-05 17:51:14 -0300

    Organizando dataloaders

--- src/graphql/user/dataloaders.js
+++ src/graphql/user/dataloaders.js
@@ -2,7 +2,6 @@ import DataLoader from 'dataloader';
 
 export const makeuserDataLoader = (getUsers) => {
   return new DataLoader(async (ids) => {
-    console.log(ids);
     const urlQuery = ids.join('&id=');
     const response = await getUsers('?id=' + urlQuery);
     const users = await response.json();
