3fbec259baf65f676f837b9bff865bf0ad682fc2
Gustavoph <gusta.coliveira07@gmail.com>
2021-08-05 23:47:42 -0300

    Juntando dataloader

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -1,5 +1,4 @@
 import fetch from 'node-fetch';
-import { makePostDataLoader } from './post/dataloaders';
 import { getPosts } from './post/utils';
 import { makeuserDataLoader } from './user/dataloaders';
 import { getUsers } from './user/utils';
@@ -7,7 +6,6 @@ import { getUsers } from './user/utils';
 export const context = () => {
   return {
     userDataLoader: makeuserDataLoader(getUsers(fetch)),
-    postDataLoader: makePostDataLoader(getPosts(fetch)),
     getUsers: getUsers(fetch),
     getPosts: getPosts(fetch),
   };
